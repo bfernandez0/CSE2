0191db063f6fba3a0f975dc458c146cb5fc2e86c
bfernandez0 <bsf319@lehigh.edu>
2015-08-31 20:02:31 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+// Brian Fernandez
+// Demo 0
+// Welcome Class Java Program
+//
+//   first compile the prgram
+//      javac WelcomeClass.java
+//   run the program
+//      WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method 
+public static void main(String[] args) {
+
+//  print the statemnet with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("| -B--S--F--3--1--9- |");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This May my family and I went to Ecuador to visit family.");
+    System.out.println("test");
+    
+    //test/testtest
+  }
+}
